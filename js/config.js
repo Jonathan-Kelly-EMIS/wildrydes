b8cda350a5cbc4ff4326b918ee5af57be0767fde
jonathan-kelly-emis <jonathan.kelly@emishealth.com>
2020-02-13 15:40:55 +0000

    API added

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-west-2'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://2h4sfrx5jh.execute-api.eu-west-2.amazonaws.com/production',
     }
 };
