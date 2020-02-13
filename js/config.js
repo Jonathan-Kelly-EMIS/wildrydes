629d774ec9701604bf3aa9c54e928a06ef303c91
jonathan-kelly-emis <jonathan.kelly@emishealth.com>
2020-02-13 14:45:28 +0000

    added valydation

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '', eu-west-2_4lg7IuMmq
+        userPoolClientId: '', 8hl5sl7vn0vkes0hfgr7j1qfo
+        region: '' eu-west-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
