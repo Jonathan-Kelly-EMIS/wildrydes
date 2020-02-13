184557059e37327c1ae859c3418fac7cb2f0c0ba
jonathan-kelly-emis <jonathan.kelly@emishealth.com>
2020-02-13 14:52:33 +0000

    I'm a tard

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', eu-west-2_4lg7IuMmq
-        userPoolClientId: '', 8hl5sl7vn0vkes0hfgr7j1qfo
-        region: '' eu-west-2
+        userPoolId: 'eu-west-2_4lg7IuMmq', 
+        userPoolClientId: '8hl5sl7vn0vkes0hfgr7j1qfo',
+        region: 'eu-west-2'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
